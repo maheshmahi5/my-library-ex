70cdba70d84935132a894a49edc1a0a51f533b31
uma mahesh <maheshbora13@gmail.com>
2019-07-01 13:36:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './models/app.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exercise3';
+  //title = 'exercise3';
+  books:Book[] = [];
+  book:Book;
+  newtitle:string;
+  newauthor:string;
+  
+  addtitle(event: any)
+  {
+    this.newtitle=event.target.value;
+  }
+  addauthor(event: any)
+  {
+    this.newauthor=event.target.value;
+  }
+  Addbook()
+  {
+    this.book={
+    title :"",
+    author : ""
+    };
+    this.book.title=this.newtitle;
+    this.book.author=this.newauthor;
+    this.books.push(this.book);
+
+  }
+  delete(data:number)
+  {
+    this.books.splice(data,1);
+  }
 }
